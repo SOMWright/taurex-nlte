6b0e07e71ba971e8fd25bafa6916740ece2c8a7a
Sam Wright <SOMWright@users.noreply.github.com>
2021-11-25 11:40:28 +0000

    Add initial plugin structure and NLTEOpacity class

---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import find_packages
+from distutils.core import setup
+
+packages = find_packages(exclude=('tests', 'doc'))
+provides = ['taurex_nlte', ]
+
+requires = []
+
+install_requires = ['taurex', ]
+
+entry_points = {'taurex.plugins': 'nlte = taurex_nlte'}
+
+setup(name='taurex_nlte',
+      author="Sam Wright",
+      author_email="samuel.wright.13@ucl.ac.uk",
+      license="BSD",
+      description='A Taurex3 plugin to implement non-LTE functionality based on bi-temperature approximated cross sections',
+      packages=packages,
+      entry_points=entry_points,
+      provides=provides,
+      requires=requires,
+      install_requires=install_requires)
---
+++ taurex_nlte/opacities/NLTEOpacity.py
@@ -0,0 +1,29 @@
+import numpy as np
+from taurex.opacity import Opacity
+
+
+class NLTEOpacity(Opacity):
+    @property
+    def vibTemperatureGrid(self):
+        return None
+
+    def compute_opacity(self, temperature, pressure, wngrid=None, temperature_vib=None):
+        """
+        Must return in units of cm2
+        """
+        raise NotImplementedError
+
+    def opacity(self, temperature, pressure, wngrid=None, temperature_vib=None):
+
+        if wngrid is None:
+            wngrid_filter = slice(None)
+        else:
+            wngrid_filter = np.where((self.wavenumberGrid >= wngrid.min()) & (
+                    self.wavenumberGrid <= wngrid.max()))[0]
+
+        orig = self.compute_opacity(temperature, pressure, wngrid_filter, temperature_vib=temperature_vib)
+
+        if wngrid is None or np.array_equal(self.wavenumberGrid.take(wngrid_filter), wngrid):
+            return orig
+        else:
+            return np.interp(wngrid, self.wavenumberGrid[wngrid_filter], orig)
